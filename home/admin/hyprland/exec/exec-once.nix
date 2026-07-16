8bcb79e20950c0e7edb44fa742ddc0f1953d3cde
Thomas Gauthier <thomas.gauthier.1@tuta.com>
2026-07-16 15:24:36 -0400

    fix(start): zen spawning on workspace 1
    Signed-off-by: Thomas Gauthier <thomas.gauthier.1@tuta.com>

--- home/admin/hyprland/exec/exec-once.nix
+++ home/admin/hyprland/exec/exec-once.nix
@@ -8,20 +8,28 @@ let
   onStart = { cmd, rules ? "" }: {
     _args = [
       "hyprland.start"
-      (pkgs.lib.generators.mkLuaInline "function() hl.exec_cmd(${fls.utils.listToLuaString [cmd]}, {${rules}}) end")
+      (pkgs.lib.generators.mkLuaInline "function() hl.exec_cmd(${fls.utils.listToLuaString cmd}, {${rules}}) end")
     ];
   };
 in
 {
   wayland.windowManager.hyprland = {
-    settings.on = builtins.map (onStart) [
-      { cmd = "ags run"; }
-      { cmd = "hyprctl setcursor 'Nordzy-cursors' 24"; }
+    extraConfig = ''
+      sub  = hl.on("window.open", function(wi)
+        if wi.class == "app.zen_browser.zen" then
+          hl.dispatch(hl.dsp.window.move({ window = wi.id, workspace = "2" }))
+          sub:remove()
+        end
+      end)
+    '';
 
-      { cmd = "hyprctl dispatch workspace 2"; }
-      { cmd = { val = "status"; };  rules = "workspace = \"1 silent\""; }
-      { cmd = { val = "browser"; }; rules = "workspace = \"2 silent\""; }
-      { cmd = { val = "term"; };    rules = "workspace = \"3 silent\""; }
+    settings.on = builtins.map (onStart) [
+      { cmd = ["ags run"]; }
+      { cmd = ["hyprctl setcursor 'Nordzy-cursors' 24"]; }
+
+      { cmd = [{ val = "status"; }];  rules = "workspace = \"1 silent\""; }
+      { cmd = [{ val = "term"; }];    rules = "workspace = \"3 silent\""; }
+      { cmd = [{ val = "browser"; }]; }
     ];
   };
 }
