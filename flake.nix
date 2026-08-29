48010fc1c807f725cca97f160417dd79ed537f26
Thomas Gauthier <thomas.gauthier.1@tuta.com>
2026-08-29 09:49:55 -0400

    fix(flake): unused inputs
    Signed-off-by: Thomas Gauthier <thomas.gauthier.1@tuta.com>

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
-# Flakes configuration. Specifies the input urls and
-# external packages.
-#
-# This should not make any changes to configuration files,
-# only import them.
 {
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "path:/home/admin/docs/code/nixpkgs";
 
-    flake-utils.url = "github:numtide/flake-utils";
-    hyprland.url    = "github:hyprwm/Hyprland";
-    nix-flatpak.url = "https://flakehub.com/f/gmodena/nix-flatpak/0.7.0";
+    nix-flatpak.url  = "https://flakehub.com/f/gmodena/nix-flatpak/0.7.0";
+
+    notifs-piper = {
+      url = "github:xdguser1/notifs-piper";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     astal = {
       url = "github:aylur/astal";
@@ -33,7 +36,7 @@
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
+    notifs-piper,
     home-manager,
     hyprland,
     nix-flatpak,
@@ -48,16 +51,9 @@
           ./configuration.nix
           home-manager.nixosModules.home-manager
           nix-flatpak.nixosModules.nix-flatpak
+          notifs-piper.nixosModules.default
           {
             nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
-             # Quote from albert's license:
-             #
-             # > This is basically a BSD-2-Clause without the permission for redistribution
-             # > of modifications and an explicit exclusion of Microsoft or Apple platforms.
-             #
-             # So, even if nixpkgs considers this unfree, it can be tolerated.
-             # Plus, it's a rare linux exclusive, which is always fun.
-             "albert"
              "cmd-parser.nvim"
            ];
           }
@@ -85,11 +81,11 @@
 
         specialArgs = {
            inherit
-           flake-utils
            home-manager
            hyprland
            nix-flatpak
            ags
+           notifs-piper
            astal;
            inherit (self) inputs;
 
